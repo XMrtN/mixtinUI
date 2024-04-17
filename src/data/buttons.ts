70c9f9aa5f94e5493dce737959c7d2a8c74b1d86
XMrtN <sepulvedamartin072@gmail.com>
2024-04-16 22:08:38 -0300

    documentation page and components

---
+++ src/data/buttons.ts
@@ -0,0 +1,23 @@
+import Button1 from "@/components/buttons/Button1.astro";
+import Button2 from "@/components/buttons/Button2.astro";
+
+export const BUTTONS = [
+  {
+    id: "button1",
+    Component: Button1,
+    code: {
+      reactCSS: [],
+      reactTailwind: [],
+      htmlCSS: [],
+    },
+  },
+  {
+    id: "button2",
+    Component: Button2,
+    code: {
+      reactCSS: [],
+      reactTailwind: [],
+      htmlCSS: [],
+    },
+  },
+];
