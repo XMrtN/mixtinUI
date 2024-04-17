1189393364dbb9f187a21d7c7a0704b47fab831b
XMrtN <sepulvedamartin072@gmail.com>
2024-04-16 22:04:29 -0300

    change hover variant name

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 const plugin = require("tailwindcss/plugin");
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "selector",
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		plugin(function ({ addVariant }) {
-      addVariant("hover", "@media (any-hover: hover) { &:hover }");
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.neutral,
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("hover-hover", "@media (any-hover: hover) { &:hover }");
       addVariant(
-        "group-hover",
+        "group-hover-hover",
         "@media (any-hover: hover) { :merge(.group):hover & }",
       );
       addVariant("hover-active", [
@@ -22,6 +27,6 @@ export default {
         "@media (any-hover: hover) { :merge(.group):hover & }",
         ":merge(.group):active &",
       ]);
-    })
-	],
-}
+    }),
+  ],
+};
