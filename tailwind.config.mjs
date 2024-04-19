cde32730f18750d08c44552f49f49d0089609758
XMrtN <sepulvedamartin072@gmail.com>
2024-04-18 22:46:16 -0300

    replace gray color by neutral color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,11 +6,7 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "selector",
   theme: {
-    extend: {
-      colors: {
-        gray: colors.neutral,
-      },
-    },
+    extend: {},
   },
   plugins: [
     plugin(function ({ addVariant }) {
