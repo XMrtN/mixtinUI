81a0d4a671e3c3d74a12a6c121d69f5ed68bc64f
XMrtN <sepulvedamartin072@gmail.com>
2024-04-30 16:49:35 -0300

    remove utils and tailwind variants

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
-const plugin = require("tailwindcss/plugin");
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -8,21 +6,4 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    plugin(function ({ addVariant }) {
-      addVariant("hover-hover", "@media (any-hover: hover) { &:hover }");
-      addVariant(
-        "group-hover-hover",
-        "@media (any-hover: hover) { :merge(.group):hover & }",
-      );
-      addVariant("hover-active", [
-        "@media (any-hover: hover) { &:hover }",
-        "&:active",
-      ]);
-      addVariant("group-hover-active", [
-        "@media (any-hover: hover) { :merge(.group):hover & }",
-        ":merge(.group):active &",
-      ]);
-    }),
-  ],
 };
