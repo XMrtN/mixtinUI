cd00141ad83191448ab6d3944919b3ff304180b4
XMrtN <sepulvedamartin072@gmail.com>
2024-04-30 16:51:15 -0300

    media queries for mobile

--- src/components/app/CodeBlock.jsx
+++ src/components/app/CodeBlock.jsx
@@ -1,4 +1,4 @@
-import { useState } from "preact/hooks";
+import { useState } from "react";
 
 export default function CodeBlock(props) {
   const [copied, setCopied] = useState(false);
@@ -11,12 +11,12 @@ export default function CodeBlock(props) {
 
   return (
     <div className="group relative mt-8 font-mono text-sm">
-      <span className="pointer-events-none absolute right-2 top-2 select-none text-xs font-semibold text-neutral-400 transition-opacity group-hover:opacity-0">
+      <span className="pointer-events-none absolute right-2 top-2 select-none text-xs font-semibold text-neutral-400 transition-opacity max-sm:-translate-x-12 sm:group-hover:opacity-0">
         {props.title}
       </span>
       <button
         type="button"
-        className="absolute right-2 top-2 flex rounded-md border border-neutral-700 bg-neutral-800 p-2 text-neutral-400 opacity-0 transition-opacity group-hover:opacity-100"
+        className="absolute right-2 top-2 flex rounded-md border border-neutral-700 bg-neutral-800 p-2 text-neutral-400 transition-opacity sm:opacity-0 sm:group-hover:opacity-100"
         onClick={onCopy}
       >
         {copied ? props.checkIcon : props.copyIcon}
