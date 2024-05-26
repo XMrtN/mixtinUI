59cd906fa693723c76adceb87b95c75f95950e0b
XMrtN <sepulvedamartin072@gmail.com>
2024-05-26 12:54:22 -0300

    add html+css+js tabs variant

--- src/data/tabs/tab1.ts
+++ src/data/tabs/tab1.ts
@@ -3,7 +3,7 @@ export const TAB1 = [
     title: "HTML + TailwindCSS + JS",
     code: `<div
   id="tabs1"
-  class="flew-row relative mx-auto flex h-12 overflow-hidden rounded-3xl border border-black/40 bg-neutral-800 px-2 backdrop-blur-sm"
+  class="relative mx-auto flex h-12 overflow-hidden rounded-3xl border border-black/40 bg-neutral-800 px-2 backdrop-blur-sm"
 >
   <span
     id="activeTab1"
@@ -11,25 +11,21 @@ export const TAB1 = [
   >
     <span class="h-full w-full rounded-3xl bg-gray-200/30"></span>
   </span>
-
   <button
     class="my-auto cursor-pointer select-none rounded-full px-4 text-center font-light text-white hover:text-neutral-300"
   >
     Home
   </button>
-
   <button
     class="my-auto cursor-pointer select-none rounded-full px-4 text-center font-light text-white hover:text-neutral-300"
   >
     About
   </button>
-
   <button
     class="my-auto cursor-pointer select-none rounded-full px-4 text-center font-light text-white hover:text-neutral-300"
   >
     Projects
   </button>
-
   <button
     class="my-auto cursor-pointer select-none rounded-full px-4 text-center font-light text-white hover:text-neutral-300"
   >
@@ -50,6 +46,102 @@ export const TAB1 = [
 
     tabs.forEach((tab) => {
       tab.classList.toggle("hover:text-neutral-300", tab !== el);
+    });
+  };
+
+  setTabPosition();
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+      setTabPosition(e);
+    });
+  });
+</script>
+`,
+  },
+  {
+    title: "HTML + CSS + JS",
+    code: `<div id="tabs1">
+  <span id="activeTab1">
+    <span></span>
+  </span>
+  <button>Home</button>
+  <button>About</button>
+  <button>Projects</button>
+  <button>Contact</button>
+</div>
+
+<style>
+  #tabs1 {
+    position: relative;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    height: 3rem;
+    overflow: hidden;
+    border-radius: 1.5rem;
+    border: 1px solid rgb(0 0 0 / 0.4);
+    background-color: #262626;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+    backdrop-filter: blur(4px);
+  }
+
+  #tabs1 button {
+    margin-top: auto;
+    margin-bottom: auto;
+    cursor: pointer;
+    user-select: none;
+    border-radius: 9999px;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    text-align: center;
+    font-weight: 300;
+    color: #fff;
+  }
+
+  #tabs1 button:not(.active):hover {
+    color: #d4d4d4;
+  }
+
+  #activeTab1 {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    top: 0;
+    z-index: -10;
+    display: flex;
+    width: 0;
+    overflow: hidden;
+    border-radius: 1.5rem;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    transition-property: left, width;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 300ms;
+  }
+
+  #activeTab1 span {
+    height: 100%;
+    width: 100%;
+    border-radius: 1.5rem;
+    background-color: rgb(229 231 235 / 0.3);
+  }
+</style>
+
+<script>
+  const tabs = document.querySelectorAll("#tabs1 button");
+  const activeTab = document.querySelector("#activeTab1");
+
+  const setTabPosition = (e) => {
+    let el = e ? e.target : tabs[0];
+    activeTab?.setAttribute(
+      "style",
+      \`left: \${el.offsetLeft}px; width: \${el.offsetWidth}px\`
+    );
+
+    tabs.forEach((tab) => {
+      tab.classList.toggle("active", tab === el);
     });
   };
 
@@ -107,7 +199,7 @@ export default function Tab1() {
   }, [activeTab]);
 
   return (
-    <div className="flew-row relative mx-auto flex h-12 overflow-hidden rounded-3xl border border-black/40 bg-neutral-800 px-2 backdrop-blur-sm">
+    <div className="relative mx-auto flex h-12 overflow-hidden rounded-3xl border border-black/40 bg-neutral-800 px-2 backdrop-blur-sm">
       <span
         className="absolute bottom-0 left-0 top-0 -z-10 flex w-0 overflow-hidden rounded-3xl py-2 transition-[left,width] duration-300"
         style={{ left: tabUnderlineLeft, width: tabUnderlineWidth }}
