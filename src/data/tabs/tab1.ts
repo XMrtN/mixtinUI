77f6cb57e2df7824689f7b90b4e275e218f0ec9c
XMrtN <sepulvedamartin072@gmail.com>
2024-05-31 17:21:06 -0300

    fix issues

--- src/data/tabs/tab1.ts
+++ src/data/tabs/tab1.ts
@@ -3,13 +3,13 @@ export const TAB1 = [
     title: "HTML + TailwindCSS + JS",
     code: `<div
   id="tabs1"
-  class="relative mx-auto flex h-12 overflow-hidden rounded-3xl border border-black/40 bg-neutral-800 px-2 backdrop-blur-sm"
+  class="relative flex h-12 overflow-hidden rounded-full border border-black/40 bg-neutral-800 px-2 backdrop-blur-sm"
 >
   <span
     id="activeTab1"
-    class="absolute bottom-0 left-0 top-0 -z-10 flex w-0 overflow-hidden rounded-3xl py-2 transition-[left,width] duration-300"
+    class="absolute bottom-0 left-0 top-0 -z-10 flex w-0 py-2 transition-[left,width] duration-300"
   >
-    <span class="h-full w-full rounded-3xl bg-gray-200/30"></span>
+    <span class="size-full rounded-full bg-gray-200/30"></span>
   </span>
   <button
     class="my-auto cursor-pointer select-none rounded-full px-4 text-center font-light text-white hover:text-neutral-300"
@@ -34,26 +34,26 @@ export const TAB1 = [
 </div>
 
 <script>
-  const tabs = document.querySelectorAll("#tabs1 button");
-  const activeTab = document.querySelector("#activeTab1");
+  const tabs1 = document.querySelectorAll("#tabs1 button");
+  const activeTab1 = document.querySelector("#activeTab1");
 
-  const setTabPosition = (e) => {
-    let el = e ? e.target : tabs[0];
-    activeTab?.setAttribute(
+  const setTabPosition1 = (e) => {
+    let el = e?.target ?? tabs1[0];
+    activeTab1?.setAttribute(
       "style",
       \`left: \${el.offsetLeft}px; width: \${el.offsetWidth}px\`
     );
 
-    tabs.forEach((tab) => {
+    tabs1.forEach((tab) => {
       tab.classList.toggle("hover:text-neutral-300", tab !== el);
     });
   };
 
-  setTabPosition();
+  setTabPosition1();
 
-  tabs.forEach((tab) => {
+  tabs1.forEach((tab) => {
     tab.addEventListener("click", (e) => {
-      setTabPosition(e);
+      setTabPosition1(e);
     });
   });
 </script>
@@ -74,12 +74,10 @@ export const TAB1 = [
 <style>
   #tabs1 {
     position: relative;
-    margin-left: auto;
-    margin-right: auto;
     display: flex;
     height: 3rem;
     overflow: hidden;
-    border-radius: 1.5rem;
+    border-radius: 9999px;
     border: 1px solid rgb(0 0 0 / 0.4);
     background-color: #262626;
     padding-left: 0.5rem;
@@ -112,8 +110,6 @@ export const TAB1 = [
     z-index: -10;
     display: flex;
     width: 0;
-    overflow: hidden;
-    border-radius: 1.5rem;
     padding-top: 0.5rem;
     padding-bottom: 0.5rem;
     transition-property: left, width;
@@ -124,32 +120,32 @@ export const TAB1 = [
   #activeTab1 span {
     height: 100%;
     width: 100%;
-    border-radius: 1.5rem;
+    border-radius: 9999px;
     background-color: rgb(229 231 235 / 0.3);
   }
 </style>
 
 <script>
-  const tabs = document.querySelectorAll("#tabs1 button");
-  const activeTab = document.querySelector("#activeTab1");
+  const tabs1 = document.querySelectorAll("#tabs1 button");
+  const activeTab1 = document.querySelector("#activeTab1");
 
-  const setTabPosition = (e) => {
-    let el = e ? e.target : tabs[0];
-    activeTab?.setAttribute(
+  const setTabPosition1 = (e) => {
+    let el = e?.target ?? tabs1[0];
+    activeTab1?.setAttribute(
       "style",
       \`left: \${el.offsetLeft}px; width: \${el.offsetWidth}px\`
     );
 
-    tabs.forEach((tab) => {
+    tabs1.forEach((tab) => {
       tab.classList.toggle("active", tab === el);
     });
   };
 
-  setTabPosition();
+  setTabPosition1();
 
-  tabs.forEach((tab) => {
+  tabs1.forEach((tab) => {
     tab.addEventListener("click", (e) => {
-      setTabPosition(e);
+      setTabPosition1(e);
     });
   });
 </script>
@@ -159,7 +155,7 @@ export const TAB1 = [
     title: "React + TailwindCSS",
     code: `import { useEffect, useRef, useState } from "react";
 
-let tabs = [
+let tabs1 = [
   {
     id: "home",
     name: "Home",
@@ -180,37 +176,37 @@ let tabs = [
 
 export default function Tab1() {
   const tabsRef = useRef([]);
-  const [activeTab, setActiveTab] = useState(0);
+  const [activeTab1, setActiveTab] = useState(0);
   const [tabUnderlineWidth, setTabUnderlineWidth] = useState(0);
   const [tabUnderlineLeft, setTabUnderlineLeft] = useState(0);
 
   useEffect(() => {
-    if (activeTab === null) {
+    if (activeTab1 === null) {
       return;
     }
 
-    const setTabPosition = () => {
-      const currentTab = tabsRef.current[activeTab];
+    const setTabPosition1 = () => {
+      const currentTab = tabsRef.current[activeTab1];
       setTabUnderlineLeft(currentTab.offsetLeft ?? 0);
       setTabUnderlineWidth(currentTab.clientWidth ?? 0);
     };
 
-    setTabPosition();
-  }, [activeTab]);
+    setTabPosition1();
+  }, [activeTab1]);
 
   return (
-    <div className="relative mx-auto flex h-12 overflow-hidden rounded-3xl border border-black/40 bg-neutral-800 px-2 backdrop-blur-sm">
+    <div className="relative flex h-12 overflow-hidden rounded-full border border-black/40 bg-neutral-800 px-2 backdrop-blur-sm">
       <span
-        className="absolute bottom-0 left-0 top-0 -z-10 flex w-0 overflow-hidden rounded-3xl py-2 transition-[left,width] duration-300"
+        className="absolute bottom-0 left-0 top-0 -z-10 flex w-0 py-2 transition-[left,width] duration-300"
         style={{ left: tabUnderlineLeft, width: tabUnderlineWidth }}
       >
-        <span className="h-full w-full rounded-3xl bg-gray-200/30" />
+        <span className="size-full rounded-full bg-gray-200/30" />
       </span>
-      {tabs.map((tab, index) => (
+      {tabs1.map((tab, index) => (
         <button
           key={index}
           ref={(el) => (tabsRef.current[index] = el)}
-          className={\`my-auto cursor-pointer select-none rounded-full px-4 text-center font-light text-white \${activeTab === index ? "" : "hover:text-neutral-300"}\`}
+          className={\`my-auto cursor-pointer select-none rounded-full px-4 text-center font-light text-white \${activeTab1 === index ? "" : "hover:text-neutral-300"}\`}
           onClick={() => setActiveTab(index)}
         >
           {tab.name}
