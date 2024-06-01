29477f170209866217ada84635fdc65a43a67bc1
XMrtN <sepulvedamartin072@gmail.com>
2024-05-31 22:08:25 -0300

    some changes

--- src/components/app/Prueba.jsx
+++ src/components/app/Prueba.jsx
@@ -1,56 +1,27 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
-let slides = [
-  "ITEM 1",
-  "ITEM 2",
-  "ITEM 3",
-  "ITEM 4",
-  "ITEM 5",
-  "ITEM 6",
-  "ITEM 7",
-  "ITEM 8",
-  "ITEM 9",
-  "ITEM 10",
-];
-
-export default function Slider1() {
-  const imageListRef = useRef([]);
-  const [activeTab, setActiveTab] = useState(0);
-  const [tabUnderlineWidth, setTabUnderlineWidth] = useState(0);
-  const [tabUnderlineLeft, setTabUnderlineLeft] = useState(0);
-
-  useEffect(() => {
-    if (activeTab === null) {
-      return;
-    }
-
-    const setTabPosition = () => {
-      const currentTab = tabsRef.current[activeTab];
-      setTabUnderlineLeft(currentTab.offsetLeft ?? 0);
-      setTabUnderlineWidth(currentTab.clientWidth ?? 0);
-    };
-
-    setTabPosition();
-  }, [activeTab]);
+export default function Dropdown() {
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className="relative mx-auto flex h-12 overflow-hidden rounded-3xl border border-black/40 bg-neutral-800 px-2 backdrop-blur-sm">
-      <span
-        className="absolute bottom-0 left-0 top-0 -z-10 flex w-0 overflow-hidden rounded-3xl py-2 transition-[left,width] duration-300"
-        style={{ left: tabUnderlineLeft, width: tabUnderlineWidth }}
+    <div className="relative z-[999]">
+      <button
+        className="relative z-10 inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 transition-transform active:scale-95"
+        onClick={() => setOpen(!open)}
       >
-        <span className="h-full w-full rounded-3xl bg-gray-200/30" />
-      </span>
-      {tabs.map((tab, index) => (
-        <button
-          key={index}
-          ref={(el) => (tabsRef.current[index] = el)}
-          className={`my-auto cursor-pointer select-none rounded-full px-4 text-center font-light text-white ${activeTab === index ? "" : "hover:text-neutral-300"}`}
-          onClick={() => setActiveTab(index)}
-        >
-          {tab.name}
-        </button>
-      ))}
+        Click me
+      </button>
+
+      <section
+        className={`absolute left-0 top-14 z-10 origin-top-left rounded-md bg-neutral-950 px-4 py-3 text-neutral-50 transition-[transform,opacity] duration-300 ${!open ? "pointer-events-none scale-50 select-none opacity-0" : ""}`}
+      >
+        Hello world!
+      </section>
+
+      <section
+        className={`fixed left-0 top-0 h-[1000vh] w-[200%] ${!open ? "pointer-events-none" : ""}`}
+        onClick={() => setOpen(false)}
+      />
     </div>
   );
 }
