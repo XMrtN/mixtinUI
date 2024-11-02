d51b8f2395cd01097bb154ccdcb9ff3b3d102bf6
XMrtN <sepulvedamartin072@gmail.com>
2024-11-02 00:56:55 -0300

    remove data files since data is in seed.ts

--- src/data/carousels.ts
+++
@@ -1,8 +0,0 @@
-import Carousel from "@/components/icons/carousel.astro";
-import { CAROUSEL1 } from "./carousels/carousel1";
-import { CAROUSEL2 } from "./carousels/carousel2";
-
-export const carousels = {
-  Icon: Carousel,
-  components: [CAROUSEL1, CAROUSEL2],
-};
