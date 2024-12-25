f5c17b10dac9a3140d221095be4fb04e86f9c546
XMrtN <sepulvedamartin072@gmail.com>
2024-12-25 16:16:34 -0300

    add database schemas to fix errors

---
+++ db/config.ts
@@ -0,0 +1,31 @@
+import { column, defineDb, defineTable } from "astro:db";
+
+const createTable = () =>
+  defineTable({
+    columns: {
+      id: column.number({ primaryKey: true }),
+      tailwindCode: column.text({ multiline: true, optional: true }),
+      cssCode: column.text({ multiline: true, optional: true }),
+      reactCode: column.text({ multiline: true, optional: true }),
+    },
+  });
+
+const Accordions = createTable();
+const Breadcrumbs = createTable();
+const Buttons = createTable();
+const Carousels = createTable();
+const Dropdowns = createTable();
+const Modals = createTable();
+const Tabs = createTable();
+
+export default defineDb({
+  tables: {
+    Accordions,
+    Breadcrumbs,
+    Buttons,
+    Carousels,
+    Dropdowns,
+    Modals,
+    Tabs,
+  },
+});
